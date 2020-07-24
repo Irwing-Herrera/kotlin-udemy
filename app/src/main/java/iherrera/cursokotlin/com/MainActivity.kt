0da7209f64fea739a34c2675c833afb27dde0574
irwing.herrera <irwing.herrera@kairosds.com>
2020-07-24 11:34:37 -0500

    Variables, Operators y Nullable

--- app/src/main/java/iherrera/cursokotlin/com/MainActivity.kt
+++ app/src/main/java/iherrera/cursokotlin/com/MainActivity.kt
@@ -2,10 +2,13 @@ package iherrera.cursokotlin.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import iherrera.cursokotlin.com.Others.Variables
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Variables().showCases()
     }
 }
---
+++ app/src/main/java/iherrera/cursokotlin/com/Others/Other.java
@@ -0,0 +1,7 @@
+package iherrera.cursokotlin.com.Others;
+
+public class Other {
+    public static Exception getNPE() {
+        return new NullPointerException();
+    }
+}
