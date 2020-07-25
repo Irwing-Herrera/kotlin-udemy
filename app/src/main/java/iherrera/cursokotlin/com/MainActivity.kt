b92a984c0e19c372349712d3a974832564e960f8
irwing.herrera <irwing.herrera@kairosds.com>
2020-07-25 11:15:13 -0500

    onBackPressed

--- app/src/main/java/iherrera/cursokotlin/com/MainActivity.kt
+++ app/src/main/java/iherrera/cursokotlin/com/MainActivity.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
+import android.widget.Toast
 import iherrera.cursokotlin.com.Section_01.Activities.LifeCycleEventsActivity
 import iherrera.cursokotlin.com.Section_01.Activities.ToastAndSnackBarActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var _exitEnabled: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +27,14 @@ class MainActivity : AppCompatActivity() {
 
     private inline fun <reified T> openActivity(context: Context) {
         startActivity(Intent(context, T::class.java))
+    }
+
+    override fun onBackPressed() {
+        if (_exitEnabled)
+            super.onBackPressed()
+
+        _exitEnabled = true
+        Toast.makeText(this, "Click back again to exit this screen", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed({ _exitEnabled = false }, 2000)
     }
 }
